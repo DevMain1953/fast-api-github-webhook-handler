4a4002b057e67936c1edb34c7178f8b07650fc67
DevMain1953 <alexanderbrenenburgh@gmail.com>
2023-08-06 04:27:38 +0300

    renamed files according to PEP 8, added functionality to handle push event, updated readme file

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import os
 from services import GitHubEventHandler
 
 app = FastAPI()
-event_handler = GitHubEventHandler.GitHubEventHandler()
+event_handler = GitHubEventHandler()
 
 
 @app.post("/hook")
@@ -12,5 +12,7 @@ async def receive_web_hook_from_github(request: Request):
     github_event = request.headers.get("X-Github-Event")
     if github_event == "star":
         await event_handler.handle_star_event(request)
-    elif github_event == "pull_request":
+    if github_event == "pull_request":
         await event_handler.handle_pull_request_event(request)
+    if github_event == "push":
+        await event_handler.handle_push_event(request)
